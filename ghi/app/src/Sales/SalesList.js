32469bf2cb9354eb531029362bd1d2f673c91cae
Sean Kennedy <snkennedy21@gmail.com>
2022-09-13 18:34:56 -0400

    add filter to sales list that filters by the salesperson

--- ghi/app/src/Sales/SalesList.js
+++ ghi/app/src/Sales/SalesList.js
@@ -5,12 +5,21 @@ class SalesList extends React.Component {
     super();
     this.state = {
       sales: [],
+      sales_people: [],
     };
   }
 
   async componentDidMount() {
     const salesUrl = "http://localhost:8090/api/sales/";
     const response = await fetch(salesUrl);
+
+    const salesPeopleUrl = "http://localhost:8090/api/sales-person/";
+    const salesPeopleResponse = await fetch(salesPeopleUrl);
+
+    if (salesPeopleResponse.ok) {
+      const data = await salesPeopleResponse.json();
+      this.setState({ sales_people: data });
+    }
 
     if (response.ok) {
       const data = await response.json();
@@ -19,10 +28,39 @@ class SalesList extends React.Component {
     }
   }
 
+  async selectChangeHander(e) {
+    const salesUrl = "http://localhost:8090/api/sales/";
+    const personsSalesUrl = `http://localhost:8090/api/sales-person/${e.target.value}/sales/`;
+
+    if (e.target.value === "All Sales" || e.target.value === "Choose One") {
+      const allSalesResponse = await fetch(salesUrl);
+      const allSalesData = await allSalesResponse.json();
+      this.setState({ sales: allSalesData });
+    } else {
+      const personsSalesResponse = await fetch(personsSalesUrl);
+      const personsSalesData = await personsSalesResponse.json();
+      this.setState({ sales: personsSalesData });
+    }
+  }
+
   render() {
     return (
       <React.Fragment>
         <h1>List of Sales</h1>
+        <select onChange={this.selectChangeHander.bind(this)}>
+          <option>Choose One</option>
+          <option>All Sales</option>
+          {this.state.sales_people.map((salesPerson) => {
+            return (
+              <option
+                key={salesPerson.employee_number}
+                value={salesPerson.employee_number}
+              >
+                {salesPerson.name}
+              </option>
+            );
+          })}
+        </select>
         <table className="table table-striped">
           <thead>
             <tr>
